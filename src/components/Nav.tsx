e1422cd76725d7762645e58d7eb60660943f3dbb
Wambui <petynillah@gmail.com>
2026-06-25 12:55:48 +0300

    env changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import logo from '../assets/logo.jpg'
 
 function Nav(){
+    const loginUrl= import.meta.env.VITE_PROFILE_APP_URL;
     return (
         <section className="nav">
             <img src={logo}/>
@@ -8,7 +9,7 @@ function Nav(){
             <li><a href="/">Home</a></li>
             <li><a href="/history">History</a></li>
             <li><a href="/services">Services offered $ eligibility</a></li>
-            <li><a href="http://localhost:5174/studentlogin" target="_blank">Login/Registration</a></li>
+            <li><a href={`${loginUrl}/studentlogin`} rel='noopener noreferrer' target="_blank">Login/Registration</a></li>
         </ul>
         </section>
     )
