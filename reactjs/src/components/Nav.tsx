76962ae1eb007f73cf73c8725b4613acf9862146
Wambui <petynillah@gmail.com>
2026-06-26 10:07:16 +0300

    changes

--- reactjs/src/components/Nav.tsx
+++ reactjs/src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import logo from '../assets/logo.jpg'
 
 function Nav(){
+
     return (
         <section className="nav">
             <img src={logo}/>
@@ -8,7 +9,7 @@ function Nav(){
             <li><a href="/">Home</a></li>
             <li><a href="/history">History</a></li>
             <li><a href="/services">Services offered $ eligibility</a></li>
-            <li><a href="http://localhost:5174/studentlogin" target="_blank">Login/Registration</a></li>
+            <li><a href="https://library-login.vercel.app/studentlogin" target="_blank">Login/Registration</a></li>
         </ul>
         </section>
     )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,8 +8,8 @@ function Nav(){
         <ul> 
             <li><a href="/">Home</a></li>
             <li><a href="/history">History</a></li>
-            <li><a href="/services">Services offered $ eligibility</a></li>
-            <li><a href={`${loginUrl}/studentlogin~`} rel='noopener noreferrer' target="_blank">Login/Registration</a></li>
+            <li><a href="/services">Services offered & eligibility</a></li>
+            <li><a href={`${loginUrl}/studentlogin`} rel='noopener noreferrer' target="_blank">Login/Registration</a></li>
         </ul>
         </section>
     )
