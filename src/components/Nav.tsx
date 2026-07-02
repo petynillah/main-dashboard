5c3f6a781b601485e32cb289e2d31630c08b63dc
Wambui <petynillah@gmail.com>
2026-07-02 14:02:13 +0300

    changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,8 @@ function Nav(){
             <li><a href="/">Home</a></li>
             <li><a href="/history">History</a></li>
             <li><a href="/services">Services offered & eligibility</a></li>
-            <li><a href="https://library-login.vercel.app/studentlogin" target="_blank">Login/Registration</a></li>
+            <li><a href="https://library-login.vercel.app/studentlogin" target="_blank">Login</a></li>
+            <li><a href="https://library-login.vercel.app/studentregister" target="_blank">Registration</a></li>
         </ul>
         </section>
     )
